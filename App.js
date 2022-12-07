e8dd9e75b4a03ddf04454998310510dd143aecc6
Akoom Joshua <sandiego2643@gmail.com>
2022-12-07 03:51:28 +0100

    fixed my carousel bug, thereby making the carousel to work perfectly

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
-('.collapse').collapse()
+var myCarousel = document.querySelector('#demo')
+var carousel = new bootstrap.Carousel(myCarousel);
 
+('.collapsed').collapse()
+$('#myCollapsible').collapse({
+toggle: true
+})
+var myCarousel = document.querySelector('#demo')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 500,
+  wrap: false
+})
 
 
 
